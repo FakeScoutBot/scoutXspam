725786b168e936198921611e8a4acb8ff0411267
FakeScoutBot <132735283+FakeScoutBot@users.noreply.github.com>
2025-06-18 17:14:24 +0530

    Update help.py

--- JARVIS/modules/help.py
+++ JARVIS/modules/help.py
@@ -1,6 +1,6 @@
 from telethon import events, Button
 from config import (
-    X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, SUDO_USERS, CMD_HNDLR as hl
+    X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, SUDO_USERS, CMD_HNDLR as hl, ACTIVE_HANDLERS
 )
 
 # Help Menu Constants
